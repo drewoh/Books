0b2fac2940bc2e50f20296d416c8d467fc8830e5
Drew Oh <drewoh23@gmail.com>
2023-05-24 15:53:08 -0400

    NextUI library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import { NextUIProvider, Button } from '@nextui-org/react';
+
 function App() {
   return (
-    <div className="App">
-      Hello, World!
-    </div>
+    <NextUIProvider>
+      <div className="App">
+        <Button auto flat>
+          Hello, World!
+        </Button>
+      </div>
+    </NextUIProvider>
   );
 }
 
