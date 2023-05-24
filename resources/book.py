ca730bb6e59502c59b61d802ef16a661e0b3df6b
Drew Oh <drewoh23@gmail.com>
2023-05-23 22:59:16 -0400

    Request for getting and deleting a book

--- resources/book.py
+++ resources/book.py
@@ -6,7 +6,20 @@ from db import db
 from models import BookModel
 from schemas import BookSchema
 
-blp = Blueprint("Books", "books", description="Operations on books")
+blp = Blueprint("books", __name__, description="Operations on books")
+
+@blp.route("/books/<string:book_id>")
+class Book(MethodView):
+    @blp.response(200, BookSchema)
+    def get(self, book_id):
+        book = BookModel.query.get_or_404(book_id)
+        return book
+
+    def delete(self, book_id):
+        book = BookModel.query.get_or_404(book_id)
+        db.session.delete(book)
+        db.session.commit()
+        return {"message": "Book deleted"}, 200
 
 @blp.route("/books")
 class BookList(MethodView):
