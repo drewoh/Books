4b542846f27000b03129662bb0504a5decb6b224
Drew Oh <drewoh23@gmail.com>
2023-05-25 12:22:14 -0400

    display books

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
 
-import { NextUIProvider, Button } from '@nextui-org/react';
+import Books from "components/Books";
+
+import { 
+  NextUIProvider, styled, Container, Text, Navbar 
+} from '@nextui-org/react';
+
+const books = [{id:1, title:"Shoe Dog", authors: "Phil Knight"}]
 
 function App() {
+  const StyledContainer = styled(Container, {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+    alignItems: "center",
+    width: "100vw",
+    height: "100vh",
+  });
+
   return (
     <NextUIProvider>
-      <div className="App">
-        <Button auto flat>
-          Hello, World!
-        </Button>
-      </div>
+      <StyledContainer>
+        <Books books={books}/>
+      </StyledContainer>
     </NextUIProvider>
   );
 }
---
+++ client/src/components/Books.tsx
@@ -0,0 +1,67 @@
+import { IBook } from "models";
+import { 
+  Card, Grid, Row, Text, Container
+} from '@nextui-org/react';
+import "./styles.css"
+
+interface IProps {
+  books: IBook[];
+}
+
+const Books = ({ books }: IProps) => {
+  const list = [
+    {
+      title: "Post Office",
+      authors: "Charles Bukowski",
+      img: "https://prodimage.images-bn.com/pimages/9780061177576_p0_v15_s192x300.jpg",
+    },
+    {
+      title: "On Writing",
+      authors: "Stephen King",
+      img: "https://prodimage.images-bn.com/pimages/9781982159375_p0_v5_s192x300.jpg",
+    },
+    {
+      title: "\"Surely You're Joking, Mr. Feynman!\"",
+      authors: "Richard P. Feynman",
+      img: "https://prodimage.images-bn.com/pimages/9780393355628_p0_v6_s192x300.jpg",
+    },
+    {
+      title: "Catch-22",
+      authors: "Joseph Heller",
+      img: "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B600%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9781451626650_p0_v4_s600x595.jpg%5D&scale=options%5Blimit%5D,size%5B600x10000%5D&sink=format%5Bwebp%5D",
+    },
+  ];
+
+ return (
+  <>
+    <Text h2>Comments on Books.</Text>
+    <Grid.Container gap={0} wrap="wrap" className="grid-container" css={{margin: "auto", padding:0, justifyContent: "space-between"}}>
+      {list.map((item, index) => (
+        <Grid key={index} className="grid-card" css={{justifyContent: "center", padding: "0", border: "none", borderShadow: "none", borderRadius: "0"}}>
+          <Card className="card" isPressable borderWeight="light" css={{maxWidth: "192px", height: "293px", border: "none", boxShadow: "none"}}>
+            <Card.Image
+              src={item.img}
+              alt={item.title}
+              className="image-container"
+            />
+            <Card.Footer className="card-footer" css={{ flexDirection: "column", alignItems: "flex-start", marginLeft: "5px"}}>
+              <Row>
+                <Text className="authors" css={{color: "$accents7", fontWeight: "$semibold", fontSize: "$sm", whiteSpace: "nowrap", overflow: "hidden", textOverflow:"ellipsis"}}>
+                  {item.authors}
+                </Text>
+              </Row>
+              <Row>
+                <Text b className="title" css={{fontSize: "$md", whiteSpace: "nowrap", overflow: "hidden", textOverflow:"ellipsis"}}>
+                  {item.title}
+                </Text>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </Grid>
+      ))}
+    </Grid.Container>
+  </>
+ )
+}
+
+export default Books
